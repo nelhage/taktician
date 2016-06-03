3f3a8976061a1863028ffa889cfe0173436a3f7e
Nelson Elhage <nelhage@nelhage.com>
2016-06-02 22:51:05 -0700

    Significant taktician-evaluate refactor

--- cmd/taktician-evaluate/main.go
+++ cmd/taktician-evaluate/main.go
@@ -6,15 +6,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"os"
 	"path"
-	"reflect"
-	"sync"
 
 	"github.com/nelhage/taktician/ai"
 	"github.com/nelhage/taktician/ptn"
-	"github.com/nelhage/taktician/tak"
 )
 
 var (
@@ -38,18 +34,6 @@ var (
 
 	out = flag.String("out", "", "directory to write ptns to")
 )
-
-type gameSpec struct {
-	i            int
-	white, black ai.TakPlayer
-	p1color      tak.Color
-}
-
-type gameResult struct {
-	spec gameSpec
-	p    *tak.Position
-	ms   []tak.Move
-}
 
 func main() {
 	flag.Parse()
@@ -90,54 +74,18 @@ func main() {
 		}
 	}
 
-	var stats [2]struct {
-		wins     int
-		flatWins int
-		roadWins int
-	}
-	var white, black int
-	var ties int
-
-	rc := make(chan gameResult)
-
-	go runGames(cfg1, cfg2, weights1, weights2, *seed, rc)
-	for r := range rc {
-		d := r.p.WinDetails()
-		if *verbose {
-			log.Printf("game n=%d plies=%d p1=%s winner=%s wf=%d bf=%d ws=%d bs=%d",
-				r.spec.i, r.p.MoveNumber(),
-				r.spec.p1color, d.Winner,
-				d.WhiteFlats,
-				d.BlackFlats,
-				r.p.WhiteStones(),
-				r.p.BlackStones(),
-			)
-		}
-		if d.Over {
-			if d.Winner == tak.White {
-				white++
-			} else if d.Winner == tak.Black {
-				black++
-			}
-			st := &stats[0]
-			if d.Winner == r.spec.p1color.Flip() {
-				st = &stats[1]
-			}
-			st.wins++
-			switch d.Reason {
-			case tak.FlatsWin:
-				st.flatWins++
-			case tak.RoadWin:
-				st.roadWins++
-			}
-		}
-		if d.Winner == tak.NoColor {
-			ties++
-		}
-		if *out != "" {
-			writeGame(*out, &r)
-		}
-	}
+	st := Simulate(&Config{
+		Cfg1:    cfg1,
+		Cfg2:    cfg2,
+		W1:      weights1,
+		W2:      weights2,
+		Games:   *games,
+		Threads: *threads,
+		Seed:    *seed,
+		Cutoff:  *cutoff,
+		Limit:   *limit,
+		Perturb: *perturb,
+	})
 
 	var j []byte
 	j, _ = json.Marshal(&weights1)
@@ -151,26 +99,26 @@ func main() {
 		log.Printf("p2c=!%s", *c2)
 	}
 	log.Printf("done games=%d seed=%d ties=%d p1.wins=%d (%d road/%d flat) p2.wins=%d (%d road/%d flat)",
-		*games, *seed, ties,
-		stats[0].wins, stats[0].roadWins, stats[0].flatWins,
-		stats[1].wins, stats[1].roadWins, stats[1].flatWins)
-	a, b := int64(stats[0].wins), int64(stats[1].wins)
+		*games, *seed, st.Ties,
+		st.Players[0].Wins, st.Players[0].RoadWins, st.Players[0].FlatWins,
+		st.Players[1].Wins, st.Players[1].RoadWins, st.Players[1].FlatWins)
+	a, b := int64(st.Players[0].Wins), int64(st.Players[1].Wins)
 	if a < b {
 		a, b = b, a
 	}
 	log.Printf("white=%d black=%d (%.2f)",
-		white, black, float64(white)/float64(white+black))
+		st.White, st.Black, float64(st.White)/float64(st.White+st.Black))
 	log.Printf("p[one-sided]=%f", binomTest(a, b, 0.5))
 }
 
-func writeGame(d string, r *gameResult) {
+func writeGame(d string, r *Result) {
 	os.MkdirAll(d, 0755)
 	p := &ptn.PTN{}
 	p.Tags = []ptn.Tag{
-		{"Size", fmt.Sprintf("%d", r.p.Size())},
+		{"Size", fmt.Sprintf("%d", r.Position.Size())},
 		{"Player1", r.spec.p1color.String()},
 	}
-	for i, m := range r.ms {
+	for i, m := range r.Moves {
 		if i%2 == 0 {
 			p.Ops = append(p.Ops, &ptn.MoveNumber{Number: i/2 + 1})
 		}
@@ -178,102 +126,4 @@ func writeGame(d string, r *gameResult) {
 	}
 	ptnPath := path.Join(d, fmt.Sprintf("%d.ptn", r.spec.i))
 	ioutil.WriteFile(ptnPath, []byte(p.Render()), 0644)
-}
-
-func worker(games <-chan gameSpec, out chan<- gameResult) {
-	for g := range games {
-		var ms []tak.Move
-		p := tak.New(tak.Config{Size: *size})
-		for i := 0; i < *cutoff; i++ {
-			var m tak.Move
-			if p.ToMove() == tak.White {
-				m = g.white.GetMove(p, *limit)
-			} else {
-				m = g.black.GetMove(p, *limit)
-			}
-			p, _ = p.Move(&m)
-			ms = append(ms, m)
-			if ok, _ := p.GameOver(); ok {
-				break
-			}
-		}
-		out <- gameResult{
-			spec: g,
-			p:    p,
-			ms:   ms,
-		}
-	}
-}
-
-func perturbWeights(p float64, w ai.Weights) ai.Weights {
-	r := reflect.Indirect(reflect.ValueOf(&w))
-	typ := r.Type()
-	for i := 0; i < typ.NumField(); i++ {
-		f := typ.Field(i)
-		if f.Type.Kind() != reflect.Int {
-			continue
-		}
-		v := r.Field(i).Interface().(int)
-		adj := rand.NormFloat64() * p
-		v = int(float64(v) * (1 + adj))
-		r.Field(i).SetInt(int64(v))
-	}
-
-	j, _ := json.Marshal(&w)
-	log.Printf("perturb: %s", j)
-
-	return w
-}
-
-func runGames(cfg1, cfg2 ai.MinimaxConfig,
-	w1, w2 ai.Weights, seed int64, rc chan<- gameResult) {
-	gc := make(chan gameSpec)
-	var wg sync.WaitGroup
-	wg.Add(*threads)
-	for i := 0; i < *threads; i++ {
-		go func() {
-			worker(gc, rc)
-			wg.Done()
-		}()
-	}
-	r := rand.New(rand.NewSource(seed))
-	for g := 0; g < *games; g++ {
-		var white, black ai.TakPlayer
-		w1 := w1
-		w2 := w2
-		if *perturb != 0.0 {
-			w1 = perturbWeights(*perturb, w1)
-			w2 = perturbWeights(*perturb, w2)
-		}
-		cfg1 := cfg1
-		cfg1.Evaluate = ai.MakeEvaluator(&w1)
-		cfg1.Seed = r.Int63()
-
-		cfg2 := cfg2
-		cfg2.Evaluate = ai.MakeEvaluator(&w2)
-		cfg2.Seed = r.Int63()
-
-		p1 := ai.NewMinimax(cfg1)
-		p2 := ai.NewMinimax(cfg2)
-		seed++
-		var p1color tak.Color
-		if g%2 == 0 {
-			white, black = p1, p2
-			p1color = tak.White
-		} else {
-			black, white = p1, p2
-			p1color = tak.Black
-		}
-
-		spec := gameSpec{
-			i:       g,
-			white:   white,
-			black:   black,
-			p1color: p1color,
-		}
-		gc <- spec
-	}
-	close(gc)
-	wg.Wait()
-	close(rc)
 }
---
+++ cmd/taktician-evaluate/simulate.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/nelhage/taktician/ai"
+	"github.com/nelhage/taktician/tak"
+)
+
+type Config struct {
+	Games int
+
+	Verbose bool
+
+	Cfg1, Cfg2 ai.MinimaxConfig
+	W1, W2     ai.Weights
+
+	Threads int
+	Seed    int64
+	Cutoff  int
+	Limit   time.Duration
+	Perturb float64
+}
+
+type Stats struct {
+	Players [2]struct {
+		Wins     int
+		FlatWins int
+		RoadWins int
+	}
+	White, Black int
+	Ties         int
+
+	Games []Result
+}
+
+type gameSpec struct {
+	c            *Config
+	i            int
+	white, black ai.TakPlayer
+	p1color      tak.Color
+}
+
+type Result struct {
+	spec     gameSpec
+	Position *tak.Position
+	Moves    []tak.Move
+}
+
+func Simulate(c *Config) Stats {
+	var st Stats
+	rc := make(chan Result)
+	go startGames(c, rc)
+	for r := range rc {
+		d := r.Position.WinDetails()
+		if c.Verbose {
+			log.Printf("game n=%d plies=%d p1=%s winner=%s wf=%d bf=%d ws=%d bs=%d",
+				r.spec.i, r.Position.MoveNumber(),
+				r.spec.p1color, d.Winner,
+				d.WhiteFlats,
+				d.BlackFlats,
+				r.Position.WhiteStones(),
+				r.Position.BlackStones(),
+			)
+		}
+		if d.Over {
+			if d.Winner == tak.White {
+				st.White++
+			} else if d.Winner == tak.Black {
+				st.Black++
+			}
+		}
+		if d.Over && d.Winner != tak.NoColor {
+			pst := &st.Players[0]
+			if d.Winner == r.spec.p1color.Flip() {
+				pst = &st.Players[1]
+			}
+			pst.Wins++
+			switch d.Reason {
+			case tak.FlatsWin:
+				pst.FlatWins++
+			case tak.RoadWin:
+				pst.RoadWins++
+			}
+		}
+		if d.Winner == tak.NoColor {
+			st.Ties++
+		}
+		st.Games = append(st.Games, r)
+	}
+
+	return st
+}
+
+func startGames(c *Config, rc chan<- Result) {
+	gc := make(chan gameSpec)
+	var wg sync.WaitGroup
+	wg.Add(c.Threads)
+	for i := 0; i < c.Threads; i++ {
+		go func() {
+			worker(gc, rc)
+			wg.Done()
+		}()
+	}
+	r := rand.New(rand.NewSource(c.Seed))
+	for g := 0; g < c.Games; g++ {
+		var white, black ai.TakPlayer
+		w1 := c.W1
+		w2 := c.W2
+		if c.Perturb != 0.0 {
+			w1 = perturbWeights(c.Perturb, w1)
+			w2 = perturbWeights(c.Perturb, w2)
+		}
+		cfg1 := c.Cfg1
+		cfg1.Evaluate = ai.MakeEvaluator(&w1)
+		cfg1.Seed = r.Int63()
+
+		cfg2 := c.Cfg2
+		cfg2.Evaluate = ai.MakeEvaluator(&w2)
+		cfg2.Seed = r.Int63()
+
+		p1 := ai.NewMinimax(cfg1)
+		p2 := ai.NewMinimax(cfg2)
+		var p1color tak.Color
+		if g%2 == 0 {
+			white, black = p1, p2
+			p1color = tak.White
+		} else {
+			black, white = p1, p2
+			p1color = tak.Black
+		}
+
+		spec := gameSpec{
+			c:       c,
+			i:       g,
+			white:   white,
+			black:   black,
+			p1color: p1color,
+		}
+		gc <- spec
+	}
+	close(gc)
+	wg.Wait()
+	close(rc)
+}
+
+func worker(games <-chan gameSpec, out chan<- Result) {
+	for g := range games {
+		var ms []tak.Move
+		p := tak.New(tak.Config{Size: g.c.Cfg1.Size})
+		for i := 0; i < g.c.Cutoff; i++ {
+			var m tak.Move
+			if p.ToMove() == tak.White {
+				m = g.white.GetMove(p, g.c.Limit)
+			} else {
+				m = g.black.GetMove(p, g.c.Limit)
+			}
+			p, _ = p.Move(&m)
+			ms = append(ms, m)
+			if ok, _ := p.GameOver(); ok {
+				break
+			}
+		}
+		out <- Result{
+			spec:     g,
+			Position: p,
+			Moves:    ms,
+		}
+	}
+}
+
+func perturbWeights(p float64, w ai.Weights) ai.Weights {
+	r := reflect.Indirect(reflect.ValueOf(&w))
+	typ := r.Type()
+	for i := 0; i < typ.NumField(); i++ {
+		f := typ.Field(i)
+		if f.Type.Kind() != reflect.Int {
+			continue
+		}
+		v := r.Field(i).Interface().(int)
+		adj := rand.NormFloat64() * p
+		v = int(float64(v) * (1 + adj))
+		r.Field(i).SetInt(int64(v))
+	}
+
+	return w
+}
