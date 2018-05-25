8184ce45a939fc9a295af08e2c7593a04d604c3d
Nelson Elhage <nelhage@nelhage.com>
2018-05-25 04:10:49 +0000

    Remove logger

--- cmd/taktician/main.go
+++ cmd/taktician/main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/nelhage/taktician/cmd/internal/analyze"
 	"github.com/nelhage/taktician/cmd/internal/canonicalize"
 	"github.com/nelhage/taktician/cmd/internal/importptn"
-	"github.com/nelhage/taktician/cmd/internal/logger"
 	"github.com/nelhage/taktician/cmd/internal/openings"
 	"github.com/nelhage/taktician/cmd/internal/play"
 	"github.com/nelhage/taktician/cmd/internal/playtak"
@@ -28,7 +27,6 @@ func main() {
 	subcommands.Register(&serve.Command{}, "")
 	subcommands.Register(&play.Command{}, "")
 
-	subcommands.Register(&logger.Command{}, "")
 	subcommands.Register(&openings.Command{}, "")
 	subcommands.Register(&canonicalize.Command{}, "")
 
--- logs/repository.go
+++ logs/repository.go
@@ -2,7 +2,6 @@ package logs
 
 import (
 	"database/sql"
-	"fmt"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3" // repository assumes sqlite
@@ -10,8 +9,6 @@ import (
 
 type Repository struct {
 	db *sql.DB
-
-	insert *sql.Stmt
 }
 
 type Game struct {
@@ -30,52 +27,7 @@ func Open(db string) (*Repository, error) {
 	if err != nil {
 		return nil, err
 	}
-	_, err = sql.Exec(createGameTable)
-	if err != nil {
-		sql.Close()
-		return nil, fmt.Errorf("create game table: %v", err)
-	}
-	_, err = sql.Exec(createPlayerTable)
-	if err != nil {
-		sql.Close()
-		return nil, fmt.Errorf("create player_game table: %v", err)
-	}
-
-	repo := &Repository{db: sql}
-	repo.insert, err = sql.Prepare(insertStmt)
-	if err != nil {
-		repo.Close()
-		return nil, fmt.Errorf("prepare: %v", err)
-	}
-	return repo, nil
-}
-
-func (r *Repository) InsertGame(g *Game) error {
-	return r.insertGame(r.insert, g)
-}
-
-func (r *Repository) insertGame(stmt *sql.Stmt, g *Game) error {
-	_, err := stmt.Exec(
-		g.Day, g.ID, g.Timestamp,
-		g.Size, g.Player1, g.Player2,
-		g.Result, g.Winner, g.Moves,
-	)
-	return err
-}
-
-func (r *Repository) InsertGames(gs []*Game) error {
-	txn, err := r.db.Begin()
-	if err != nil {
-		return err
-	}
-	defer txn.Rollback()
-	stmt := txn.Stmt(r.insert)
-	for _, g := range gs {
-		if e := r.insertGame(stmt, g); e != nil {
-			return e
-		}
-	}
-	return txn.Commit()
+	return &Repository{db: sql}, nil
 }
 
 func (r *Repository) Close() {
