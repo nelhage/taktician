ea4dae4dbc7846927deba01977dbe7f9deb87ae8
Nelson Elhage <nelhage@nelhage.com>
2018-06-11 02:10:08 +0000

    port two scripts to absl.flag

--- python/scripts/train.py
+++ python/scripts/train.py
@@ -3,7 +3,6 @@ import tak.train
 import tak.proto
 import tak.model
 
-import argparse
 import csv
 import os
 import sys
@@ -12,7 +11,31 @@ import time
 import numpy as np
 import tensorflow as tf
 
-FLAGS = None
+tf.flags.DEFINE_string('corpus', default=None, help='corpus to train')
+
+tf.flags.DEFINE_integer('kernel', default=3, help='convolutional kernel size')
+tf.flags.DEFINE_integer('filters', default=16, help='convolutional filters')
+tf.flags.DEFINE_integer('layers', default=2, help='number of convolutional layers')
+tf.flags.DEFINE_integer('hidden', default=0, help='number of hidden fully-connected nodes')
+
+tf.flags.DEFINE_string('optimizer',
+                       default='GradientDescentOptimizer',
+                       help='tensorflow optimizer class')
+                       # choices=OPTIMIZERS)
+tf.flags.DEFINE_float('eta', default=0.05, help='learning rate')
+tf.flags.DEFINE_integer('lr_scale', default=1, help='scale learning rate down')
+tf.flags.DEFINE_integer('lr_interval', default=None, help='scale learning rate every N epochs')
+tf.flags.DEFINE_float('regularize', default=1e-6, help='L2 regularization scale')
+tf.flags.DEFINE_float('dropout', default=0.5, help='dropout fraction')
+tf.flags.DEFINE_integer('batch', default=100, help='batch size')
+tf.flags.DEFINE_integer('epochs', default=30, help='train epochs')
+
+tf.flags.DEFINE_string('checkpoint', default=None, help='checkpoint directory')
+tf.flags.DEFINE_string('restore', default=None, help='restore from path')
+
+tf.flags.DEFINE_boolean('symmetries', default=True, help='Add symmetries to corpus')
+
+FLAGS = tf.flags.FLAGS
 
 def main(args):
   print("Loading data...")
@@ -84,52 +107,5 @@ OPTIMIZERS = [
       issubclass(getattr(tf.train, name), tf.train.Optimizer))
 ]
 
-def arg_parser():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--corpus', type=str, default=None,
-                      help='corpus to train')
-
-  parser.add_argument('--kernel', type=int, default=3,
-                      help='convolutional kernel size')
-  parser.add_argument('--filters', type=int, default=16,
-                      help='convolutional filters')
-  parser.add_argument('--layers', type=int, default=2,
-                      help='number of convolutional layers')
-  parser.add_argument('--hidden', type=int, default=0,
-                      help='number of hidden fully-connected nodes')
-
-  parser.add_argument('--optimizer',
-                      type=str,
-                      default='GradientDescentOptimizer',
-                      help='tensorflow optimizer class',
-                      choices=OPTIMIZERS)
-  parser.add_argument('--eta', type=float, default=0.05,
-                      help='learning rate')
-  parser.add_argument('--lr_scale', type=int, default=1,
-                      help='scale learning rate down')
-  parser.add_argument('--lr_interval', type=int, default=None,
-                      help='scale learning rate every N epochs')
-  parser.add_argument('--regularize', type=float, default=1e-6,
-                      help='L2 regularization scale')
-  parser.add_argument('--dropout', type=float, default=0.5,
-                      help='L2 regularization scale')
-  parser.add_argument('--batch', type=int, default=100,
-                      help='batch size')
-  parser.add_argument('--epochs', type=int, default=30,
-                      help='train epochs')
-
-  parser.add_argument('--checkpoint', type=str, default=None,
-                      help='checkpoint directory')
-  parser.add_argument('--restore', type=str, default=None,
-                      help='restore from path')
-
-  parser.add_argument('--no-symmetries', default=True,
-                      dest='symmetries', action='store_false')
-  return parser
-
 if __name__ == '__main__':
-  parser = arg_parser()
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=unparsed)
-else:
-  FLAGS, _ = arg_parser().parse_args([])
+  tf.app.run(main=main, argv=sys.argv)
