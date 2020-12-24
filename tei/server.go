f9251b60c6681f4b0f8bbdd0251e3c80a1f8fbf5
Morten Lohne <lohnemorten@gmail.com>
2020-12-24 02:45:37 +0100

    Update time budget calculation to avoid using the entire increment. Tako would sometimes lose on time when increment ~= time left, because of this behavior

--- tei/server.go
+++ tei/server.go
@@ -139,7 +139,7 @@ func parsePosition(size int, words []string) (*tak.Position, error) {
 func calcBudget(movetime time.Duration, gametime time.Duration, inc time.Duration) time.Duration {
 	var budget time.Duration
 	if gametime != 0 {
-		budget = gametime/10 + inc
+		budget = (gametime + inc) / 10
 	}
 	if movetime > 0 && (budget == 0 || movetime < budget) {
 		budget = movetime
