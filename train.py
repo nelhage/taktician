a263b37731c679e6a19a588009bf92f2bc9727bc
Nelson Elhage <nelhage@nelhage.com>
2021-08-23 20:18:41 -0700

    update train loop

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torch
 import time
 import itertools
 
-BATCH_SIZE = 512
+BATCH_SIZE = 64
 MINIBATCH_SIZE = 4
 
 def main():
@@ -14,9 +14,11 @@ def main():
     d_head = 32,
     n_vocab = 256,
   )
+  device = 'cuda'
+
   ds = xformer.data.PileDataset('data/pile/train/00.jsonl.zst', n_ctx=cfg.n_ctx)
   loader = torch.utils.data.DataLoader(ds, batch_size=MINIBATCH_SIZE, collate_fn=xformer.data.collate_fn)
-  model = xformer.Transformer(cfg, dtype=torch.float32, device='cuda')
+  model = xformer.Transformer(cfg, dtype=torch.float32, device=device)
 
   xent = torch.nn.CrossEntropyLoss(reduction='mean')
   opt = torch.optim.Adam(model.parameters())
@@ -26,21 +28,23 @@ def main():
   data = iter(loader)
 
   start = time.time()
+  tokens = 0
 
   for step_i in itertools.count():
     avg_loss = 0.0
+    opt.zero_grad()
     for _ in range(steps_per_batch):
       batch = next(data)
-      batch = batch.cuda()
+      batch = batch.to(device)
       logits = model(batch[:, :-1])
       targets = batch[:, 1:]
       loss = xent(logits.permute(0, 2, 1), targets)
       avg_loss += loss.item()
-      opt.zero_grad()
+      tokens += batch.numel()
       loss.backward()
-      opt.step()
+    opt.step()
     now = time.time()
-    print(f"[step={step_i:06d} t={now-start:6.1f}s tokens={(step_i+1)*BATCH_SIZE*cfg.n_ctx:08d}] loss={avg_loss/steps_per_batch:2.2f}")
+    print(f"[step={step_i:06d} t={now-start:.1f}s tokens={tokens:08d}] loss={avg_loss/steps_per_batch:2.2f}")
 
 if __name__ == '__main__':
   main()
