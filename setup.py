54e7d81a328b4822785aee0bba86e405576112fb
Nelson Elhage <nelhage@nelhage.com>
2021-08-31 22:38:43 -0700

    setup py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='xformer',
+    version='0.1.0',
+    description='A toy transformer model',
+    author='Nelson Elhage',
+    author_email='nelhage@nelhage.com',
+    packages=find_packages(exclude=('tests',))
+)
