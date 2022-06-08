06d719a6eeb7c0bf47beae01cbf0e7bd3d81798c
Nelson Elhage <nelhage@nelhage.com>
2022-06-08 11:05:02 -0700

    gencorpus: more pipelined

--- cmd/internal/gencorpus/gencorpus.go
+++ cmd/internal/gencorpus/gencorpus.go
@@ -30,8 +30,8 @@ type Command struct {
 	depth   int
 	threads int
 
-	limit time.Duration
-	prove bool
+	limit    time.Duration
+	analysis string
 
 	stats  bool
 	output string
@@ -50,7 +50,7 @@ func (c *Command) SetFlags(flags *flag.FlagSet) {
 	flags.Int64Var(&c.seed, "seed", 0, "Random seed")
 	flags.IntVar(&c.threads, "threads", runtime.NumCPU(), "Number of threads")
 
-	flags.BoolVar(&c.prove, "prove", false, "Use the DFPN prover")
+	flags.StringVar(&c.analysis, "analysis", "minimax", "Analyze to use: minimax,dfpn,none")
 	flags.DurationVar(&c.limit, "limit", 5*time.Second, "Minimax time limit when scoring")
 
 	flags.BoolVar(&c.stats, "stats", false, "compute and print stats")
@@ -85,148 +85,163 @@ func (c *Command) Execute(ctx context.Context, flag *flag.FlagSet, _ ...interfac
 	var byLength []int
 	var posCount []map[uint64]int
 
+	grp, ctx := errgroup.WithContext(ctx)
+
 	games := make(chan *game)
-	go c.generateGames(ctx, games)
+	positions := make(chan *tak.Position)
 
-	var gameList []*game
+	grp.Go(func() error {
+		c.generateGames(ctx, games)
+		return nil
+	})
+	grp.Go(func() error {
+		defer close(positions)
+		rng := rand.New(rand.NewSource(c.seed))
 
-	for g := range games {
-		gameList = append(gameList, g)
-	}
-	if c.stats {
-		for _, g := range gameList {
-			moves := len(g.positions)
-			byLength = growslice(byLength, moves)
-			byLength[moves-1] += 1
-			posCount = growslice(posCount, moves+1)
-			for i, p := range g.positions {
-				if posCount[i] == nil {
-					posCount[i] = make(map[uint64]int)
+		seen := make(map[uint64]struct{})
+
+		for g := range games {
+			if c.stats {
+				moves := len(g.positions)
+				byLength = growslice(byLength, moves)
+				byLength[moves-1] += 1
+				posCount = growslice(posCount, moves+1)
+				for i, p := range g.positions {
+					if posCount[i] == nil {
+						posCount[i] = make(map[uint64]int)
+					}
+					posCount[i][p.Hash()] += 1
 				}
-				posCount[i][p.Hash()] += 1
 			}
-		}
-		for i := range byLength {
-			log.Printf("ply=%3d games=%3d uniq=%4d", i, byLength[i], len(posCount[i]))
-		}
-	}
-
-	rng := rand.New(rand.NewSource(c.seed))
-
-	positions := make(map[uint64]*tak.Position)
-
-	for _, g := range gameList {
-		// select position
-		var idx int
-		r := rng.Float64()
-		if r < 0.01 {
-			idx = int(rng.Int31n(4))
-		} else if r < 0.25 {
-			idx = 4 + int(rng.Int31n(5))
-		} else if r < 0.95 {
-			npos := len(g.positions)
-			if npos <= 9 {
+			// select position
+			var idx int
+			r := rng.Float64()
+			if r < 0.01 {
+				idx = int(rng.Int31n(4))
+			} else if r < 0.25 {
+				idx = 4 + int(rng.Int31n(5))
+			} else if r < 0.95 {
+				npos := len(g.positions)
+				if npos <= 9 {
+					continue
+				}
+				idx = 9 + int(rng.Int31n(int32(npos)-9))
+			}
+			if idx >= len(g.positions)-1 {
 				continue
 			}
-			idx = 9 + int(rng.Int31n(int32(npos)-9))
+			pos := g.positions[idx]
+			if _, ok := seen[pos.Hash()]; ok {
+				continue
+			}
+			positions <- pos
 		}
-		if idx >= len(g.positions)-1 {
-			continue
+
+		if c.stats {
+			for i := range byLength {
+				log.Printf("ply=%3d games=%3d uniq=%4d", i, byLength[i], len(posCount[i]))
+			}
 		}
-		pos := g.positions[idx]
-		positions[pos.Hash()] = pos
-	}
+		return nil
+	})
 
 	results := make(chan entry)
-	go c.evaluate(positions, results)
+	grp.Go(func() error {
+		c.evaluate(positions, results)
+		return nil
+	})
+	grp.Go(func() error {
+		defer func() {
+			for _ = range results {
+			}
+		}()
+		fh, err := os.Create(c.output)
+		if err != nil {
+			return fmt.Errorf("open %q: %w", c.output, err)
+		}
+		defer fh.Close()
+		wr := csv.NewWriter(fh)
+		defer wr.Flush()
 
-	fh, err := os.Create(c.output)
-	if err != nil {
-		log.Printf("open %q: %s", c.output, err.Error())
+		for e := range results {
+			wr.Write([]string{
+				ptn.FormatTPS(e.pos),
+				ptn.FormatMove(e.move),
+				fmt.Sprintf("%+f", e.value),
+			})
+		}
+		return nil
+	})
+
+	if err := grp.Wait(); err != nil {
+		log.Println(err.Error())
 		return subcommands.ExitFailure
-	}
-	defer fh.Close()
-	wr := csv.NewWriter(fh)
-	defer wr.Flush()
-
-	for e := range results {
-		wr.Write([]string{
-			ptn.FormatTPS(e.pos),
-			ptn.FormatMove(e.move),
-			fmt.Sprintf("%+f", e.value),
-		})
 	}
 
 	return subcommands.ExitSuccess
 }
 
-func (c *Command) evaluate(positions map[uint64]*tak.Position, results chan<- entry) {
+func (c *Command) evaluate(positions <-chan *tak.Position, results chan<- entry) {
 	defer close(results)
-	input := make(chan *tak.Position)
 	grp := errgroup.Group{}
-	grp.Go(func() error {
-		defer close(input)
-		for _, p := range positions {
-			input <- p
-		}
-		return nil
-	})
 	for i := 0; i < c.threads; i++ {
 		grp.Go(func() error {
-			var prover *prove.DFPNSolver
-			var mm *ai.MinimaxAI
-			if c.prove {
-				prover = prove.NewDFPN(&prove.DFPNConfig{
+			var analyze func(p *entry)
+			if c.analysis == "dfpn" {
+				prover := prove.NewDFPN(&prove.DFPNConfig{
 					// Attacker: tak.White,
 					TableMem: 100 * 1 << 20,
 				})
-			} else {
-				mm = ai.NewMinimax(ai.MinimaxConfig{
-					Size:     c.size,
-					TableMem: 100 * 1 << 20,
-				})
-			}
-			/*
-				proveBlack := prove.NewDFPN(&prove.DFPNConfig{
-					Attacker: tak.Black,
-					TableMem: 100 * 1 << 20,
-				})
-			*/
-			for p := range input {
-				ent := entry{pos: p}
-				if c.prove {
-					res, _ := prover.Prove(p)
+				analyze = func(e *entry) {
+					res, _ := prover.Prove(e.pos)
+					e.move = res.Move
+
 					if res.Result == prove.EvalUnknown {
 						log.Printf("unprovable! %q bounds=%d,%d",
-							ptn.FormatTPS(p),
+							ptn.FormatTPS(e.pos),
 							res.Proof,
 							res.Disproof,
 						)
-						continue
-					} else if res.Result == prove.EvalTrue {
-						ent.value = 1.0
-					} else {
-						ent.value = -1.0
 					}
-					ent.move = res.Move
-				} else {
-					ctx, cancel := context.WithTimeout(context.Background(), c.limit)
-					pv, val, _ := mm.Analyze(
-						ctx,
-						p,
-					)
-					cancel()
-					ent.move = pv[0]
-					if val > ai.WinThreshold {
-						ent.value = 1.0
-					} else if val < -ai.WinThreshold {
-						ent.value = -1.0
-					} else if val > 0 {
-						ent.value = 0.5
-					} else if val < 0 {
-						ent.value = 0.5
+					if res.Result == prove.EvalTrue {
+						e.value = 1.0
+					} else {
+						e.value = -1.0
 					}
 				}
+			} else if c.analysis == "minimax" {
+				mm := ai.NewMinimax(ai.MinimaxConfig{
+					Size:     c.size,
+					TableMem: 100 * 1 << 20,
+				})
+
+				analyze = func(e *entry) {
+					ctx, cancel := context.WithTimeout(context.Background(), c.limit)
+					defer cancel()
+					pv, val, _ := mm.Analyze(
+						ctx,
+						e.pos,
+					)
+					e.move = pv[0]
+					if val > ai.WinThreshold {
+						e.value = 1.0
+					} else if val < -ai.WinThreshold {
+						e.value = -1.0
+					} else if val > 0 {
+						e.value = 0.5
+					} else if val < 0 {
+						e.value = 0.5
+					}
+				}
+			} else if c.analysis == "none" {
+				analyze = func(e *entry) {}
+			} else {
+				log.Fatalf("unknown analysis: %q", c.analysis)
+			}
+
+			for p := range positions {
+				ent := entry{pos: p}
+				analyze(&ent)
 
 				results <- ent
 
@@ -256,9 +271,10 @@ const prime = 1099511628211
 func (c *Command) generateWorker(ctx context.Context, games chan<- *game, todo *int64, id int) {
 	rng := rand.New(rand.NewSource(prime*c.seed + int64(id)))
 	mm := ai.NewMinimax(ai.MinimaxConfig{
-		Size:  c.size,
-		Seed:  rng.Int63(),
-		Depth: c.depth,
+		Size:     c.size,
+		Seed:     rng.Int63(),
+		Depth:    c.depth,
+		TableMem: -1,
 	})
 	rnd := ai.NewRandom(rng.Int63())
 	for {
