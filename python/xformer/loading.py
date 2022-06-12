a11740c8d684412a27165b00cba9d1fb30d31ce1
Nelson Elhage <nelhage@nelhage.com>
2022-06-12 11:57:34 -0700

    load_model

---
+++ python/xformer/loading.py
@@ -0,0 +1,13 @@
+import os.path
+import yaml
+import torch
+from .model import Transformer
+
+
+def load_model(save_dir, map_location="cpu"):
+    with open(os.path.join(save_dir, "config.yaml")) as fh:
+        config = yaml.unsafe_load(fh)
+    state = torch.load(os.path.join(save_dir, "model.pt"), map_location=map_location)
+    model = Transformer(config)
+    model.load_state_dict(state)
+    return model
