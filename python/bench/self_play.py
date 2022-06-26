391a187706b46a80cde0d6336d67d8e20176ce93
Nelson Elhage <nelhage@nelhage.com>
2022-06-26 15:25:17 -0700

    multiprocessing-based self_play

--- python/bench/self_play.py
+++ python/bench/self_play.py
@@ -1,15 +1,17 @@
 import argparse
 import sys
-from functools import partial
+import traceback
 
 import tak
 from tak import mcts
 from tak.model import grpc
 from attrs import define, field
 
-import concurrent.futures
+import queue
+from torch import multiprocessing
 
 import torch
+import numpy as np
 
 import time
 
@@ -20,7 +22,7 @@ RESIGNATION_THRESHOLD = 0.95
 class Transcript:
     positions: list[tak.Position] = field(factory=list)
     moves: list[list[tak.Move]] = field(factory=list)
-    probs: list[torch.Tensor] = field(factory=list)
+    probs: list[np.ndarray] = field(factory=list)
 
 
 def self_play(engine, size=3):
@@ -43,11 +45,57 @@ def self_play(engine, size=3):
 
         log.positions.append(tree.position)
         log.moves.append([c.move for c in tree.children])
-        log.probs.append(probs)
+        log.probs.append(probs.numpy())
 
         tree = tree.children[torch.multinomial(probs, 1).item()]
 
     return log
+
+
+@define
+class Job:
+    size: int
+
+    host: str
+    port: int
+
+    simulations: int
+
+    sema: multiprocessing.Semaphore
+    queue: multiprocessing.Queue
+    shutdown: multiprocessing.Event
+
+
+def entrypoint(job: Job, id: int):
+    try:
+        run_job(job, id)
+        job.queue.close()
+        job.queue.join_thread()
+        job.shutdown.wait()
+    except Exception as ex:
+        print(f"[{id}] Process crashed: {ex}", file=sys.stderr)
+        traceback.print_exc(file=sys.stderr)
+
+
+def run_job(job: Job, id: int):
+    network = grpc.GRPCNetwork(host=job.host, port=job.port)
+
+    engine = mcts.MCTS(
+        mcts.Config(
+            network=network,
+            simulation_limit=job.simulations,
+            time_limit=0,
+        )
+    )
+
+    logs = []
+
+    while True:
+        if not job.sema.acquire(block=False):
+            break
+        logs.append(self_play(engine, job.size))
+
+    job.queue.put(logs)
 
 
 def main(argv):
@@ -89,26 +137,44 @@ def main(argv):
 
     args = parser.parse_args(argv)
 
-    network = grpc.GRPCNetwork(host=args.host, port=args.port)
-
-    engine = mcts.MCTS(
-        mcts.Config(
-            network=network,
-            simulation_limit=args.simulations,
-            time_limit=0,
-        )
+    job = Job(
+        size=args.size,
+        simulations=args.simulations,
+        host=args.host,
+        port=args.port,
+        sema=multiprocessing.Semaphore(value=args.games),
+        queue=multiprocessing.Queue(maxsize=args.threads),
+        shutdown=multiprocessing.Event(),
     )
 
     start = time.time()
-    with concurrent.futures.ThreadPoolExecutor(args.threads) as tpe:
-        futs = [
-            tpe.submit(partial(self_play, engine, args.size)) for _ in range(args.games)
-        ]
-        logs = [f.result() for f in futs]
+    processes = [
+        multiprocessing.Process(target=entrypoint, args=(job, i))
+        for i in range(args.threads)
+    ]
+    for p in processes:
+        p.start()
+
+    results = []
+    while len(results) < args.threads:
+        try:
+            result = job.queue.get(block=True, timeout=1)
+            results.append(result)
+        except queue.Empty:
+            for p in processes:
+                if p.exitcode not in [0, None]:
+                    raise RuntimeError("Process crashed!")
+
+    job.shutdown.set()
+
+    for p in processes:
+        p.join()
     end = time.time()
 
+    logs = [l for r in results for l in r]
+
     print(
-        f"done games={len(logs)} plies={sum(len(l.positions) for l in logs)} threads={args.threads} duration={end-start:.2f}"
+        f"done games={len(logs)} plies={sum(len(l.positions) for l in logs)} threads={args.threads} duration={end-start:.2f} games/s={args.games/(end-start):.1f}"
     )
 
 
