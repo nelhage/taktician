28985c4feb8b48717da44695e63d6dc5780eb29f
Nelson Elhage <nelhage@nelhage.com>
2022-07-03 15:48:24 -0700

    Refactor saving a bit

--- python/xformer/loading.py
+++ python/xformer/loading.py
@@ -13,3 +13,14 @@ def load_model(save_dir, device="cpu"):
     model = Transformer(config, device=device)
     model.load_state_dict(state)
     return model
+
+
+def save_model(model: Transformer, save_dir: str):
+    os.makedirs(save_dir, exist_ok=True)
+
+    torch.save(
+        model.state_dict(),
+        os.path.join(save_dir, "model.pt"),
+    )
+    with open(os.path.join(save_dir, "config.yaml"), "w") as fh:
+        yaml.dump(model.cfg, fh)
