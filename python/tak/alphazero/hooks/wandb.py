cd228af2df0bc043552a4bbfd2efab5b161fe52d
Nelson Elhage <nelhage@nelhage.com>
2022-10-20 08:51:59 +0200

    wandb.watch our model

--- python/tak/alphazero/hooks/wandb.py
+++ python/tak/alphazero/hooks/wandb.py
@@ -16,6 +16,7 @@ class WandB(Hook):
     job_name: Optional[str] = None
     job_id: str = field(factory=partial(secrets.token_hex, 8))
     project: str = "taktician-alphazero"
+    watch_freq: int = 10
 
     def before_run(self, state: TrainState, config):
         state.wandb = wandb.init(
@@ -25,6 +26,7 @@ class WandB(Hook):
             resume="allow",
         )
         wandb.config.update(attrs.asdict(config), allow_val_change=True)
+        wandb.watch(state.model, log="all", log_freq=self.watch_freq)
 
     def finalize(self, state: TrainState):
         state.wandb.log(
